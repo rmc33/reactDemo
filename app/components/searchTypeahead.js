58244aacefa3d5a3bc76e8511462b9687e178047
Ryan Conigliaro <rmconigliaro@gmail.com>
2017-05-14 12:45:13 -0400

    adding chosen

--- app/components/searchTypeahead.js
+++ app/components/searchTypeahead.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { connect } from 'react-redux';
-import jQuery from 'jquery/dist/jquery';
+import { Button, Grid, Row, Col, Clearfix } from 'react-bootstrap';
+import jQuery from 'bootstrap-chosen/dist/chosen.jquery-1.4.2/chosen.jquery';
 const $ = jQuery;
+import chosen from 'bootstrap-chosen/dist/chosen.jquery-1.4.2/chosen.jquery.min';
 
+const searchOptions = [
+	{id: '', value: ''}
+];
 
 export default class SearchTypeahead extends React.Component {
 
@@ -14,18 +19,19 @@ export default class SearchTypeahead extends React.Component {
 
 	componentDidMount() {
 		this.refs.search_input.focus();
+		$('#search_input').chosen();
 	}
 
 	//save changes to search input
-	 //search view search_value is now used to get value of search instead of accessing dom
-	 handleChange(event) {
-	 	 if (event) {
+	//search view search_value is now used to get value of search instead of accessing dom
+	handleChange(event) {
+	 	if (event) {
 		 	this.setState({search_value: event.target.value});
 		 	this.props.doSearch(event.target.value);
-		 }
-	 }
+		}
+	}
 
-	 displayResults() {
+	displayResults() {
 	 	if (this.props.data) {
 	 		return (
 	 			<div>
@@ -38,18 +44,22 @@ export default class SearchTypeahead extends React.Component {
 			</div>
 	 		);
 	 	}
-	 	return '';
-	 }
+	}
+
+	renderSearchOptions() {
+		return this.searchOptions.map((option) => {
+			return <option id={option.id} value={option.value}/>
+		});
+	}
 
 	render() {
-	  
-		  return (
-		    <div>
-		        <input ref="search_input" type="text" id="search_input" onChange={this.handleChange} />
-		    	searching for {this.props.search_value}
-		    	{this.displayResults()}
+		return (
+		    <div className="typeahead-container">
+		        <select ref="search_input" id="search_input" onChange={this.handleChange}>
+		        {this.renderSearchOptions()}
+		        </select>
 		    </div>
-		  );
+		);
 	}
 
 }
